8489ad33d92c8976dbcb75d2a27c80d0e4981d17
italobfa <italobfa@hotmail.com>
2022-01-15 21:01:24 -0300

    captura contatos

---
+++ app/src/main/java/com/example/contactdio/ContactAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.contactdio
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter(val contactsList: ArrayList<Contact>): RecyclerView.Adapter<ContactAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ContactAdapter.ViewHolder, position: Int) {
+        holder.bindItem(contactsList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return contactsList.size
+    }
+    class ViewHolder (itemView: View): RecyclerView.ViewHolder(itemView){
+
+        fun bindItem(contact: Contact){
+            val textName = itemView.findViewById<TextView>(R.id.contact_name)
+            val textPhoneNumber = itemView.findViewById<TextView>(R.id.contact_phone_number)
+
+            textName.text = contact.name
+            textPhoneNumber.text = contact.phoneNumber
+        }
+    }
+}
